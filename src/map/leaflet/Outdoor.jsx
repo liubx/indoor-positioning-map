b24bca0a4cbd766c7fa1f1b3801b1b91eb92f6f6
LB <liubx1988@gmail.com>
2019-03-21 07:31:15 +0800

    MOD: add leaflet.wms

--- src/map/leaflet/Outdoor.jsx
+++ src/map/leaflet/Outdoor.jsx
@@ -3,26 +3,63 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import L from 'leaflet';
+import * as WMS from 'leaflet.wms';
 import { BASE_MAP_URL } from '../constant';
 import { OUTDOOR_MAX_ZOOM, OUTDOOR_MIN_ZOOM } from './config';
 
 class LlOutdoorLayer extends Component {
   componentDidMount() {
     this.loadIndoorMaps();
+    // WMS.default.Source.extend({
+    //   ajax: function(url, callback) {
+    //     console.log(url);
+    //     // $.ajax(url, {
+    //     //     'context': this,
+    //     //     'success': function(result) {
+    //     //         callback.call(this, result);
+    //     //      }
+    //     // });
+    //   },
+    //   showFeatureInfo: function(latlng, info) {
+    //     console.log(info);
+    //   }
+    // });
   }
 
   loadIndoorMaps() {
-    L.tileLayer
-      .wms(`${BASE_MAP_URL}/indoor_map/wms`, {
-        layers: 'indoor_map:indoor_map',
-        tiled: true,
-        format: 'image/png',
-        transparent: true,
-        maxZoom: OUTDOOR_MAX_ZOOM,
-        minZoom: OUTDOOR_MIN_ZOOM,
-        continuousWorld: true
-      })
-      .addTo(this.context.map);
+    WMS.default.Source.extend({
+      ajax: function(url, callback) {
+        console.log(111);
+      },
+      showFeatureInfo: function(latlng, info) {
+        console.log(222);
+      },
+      getFeatureInfoParams: function(latlng, info) {
+        console.log(333);
+      }
+    });
+    var source = WMS.default.Source(`${BASE_MAP_URL}/indoor_map/wms`, {
+      layers: 'indoor_map:indoor_map',
+      tiled: true,
+      format: 'image/png',
+      transparent: true,
+      maxZoom: OUTDOOR_MAX_ZOOM,
+      minZoom: OUTDOOR_MIN_ZOOM,
+      continuousWorld: true
+    });
+    source.getLayer('indoor_map:indoor_map').addTo(this.context.map);
+    
+    // L.tileLayer
+    //   .wms(`${BASE_MAP_URL}/indoor_map/wms`, {
+    //     layers: 'indoor_map:indoor_map',
+    //     tiled: true,
+    //     format: 'image/png',
+    //     transparent: true,
+    //     maxZoom: OUTDOOR_MAX_ZOOM,
+    //     minZoom: OUTDOOR_MIN_ZOOM,
+    //     continuousWorld: true
+    //   })
+    //   .addTo(this.context.map);
   }
 
   render() {
