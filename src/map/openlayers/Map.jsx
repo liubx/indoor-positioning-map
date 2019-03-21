a3aabf946c9bff331a37e9b41883464ac39bb938
LB <liubx1988@gmail.com>
2019-03-21 13:44:32 +0800

    MOD: load BASE_MAP in OlOutdoorLayer

--- src/map/openlayers/Map.jsx
+++ src/map/openlayers/Map.jsx
@@ -2,8 +2,6 @@
 /* eslint no-undef: "error" */
 import React, { Component } from 'react';
 import Map from 'ol/Map';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
 import { defaults as defaultInteractions } from 'ol/interaction';
 import View from 'ol/View';
 import PropTypes from 'prop-types';
@@ -13,10 +11,8 @@ import {
   DEFAULT_PROJECTION,
   OUTDOOR_DEFAULT_ZOOM,
   OUTDOOR_MAX_ZOOM,
-  OUTDOOR_MIN_ZOOM,
-  OUTDOOR_MIN_RESOLUTION
+  OUTDOOR_MIN_ZOOM
 } from './config';
-import { TIANDITU_URL } from '../constant';
 
 class OlMapLayer extends Component {
   constructor(props) {
@@ -47,35 +43,6 @@ class OlMapLayer extends Component {
       }),
       target: this.ref,
       controls: [],
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: `${TIANDITU_URL}/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=111b0cbae5ef2fb8ebdf06f937b12dd8`,
-            projection: getProjection('EPSG:3857')
-          }),
-          minResolution: OUTDOOR_MIN_RESOLUTION
-        }),
-        new TileLayer({
-          source: new XYZ({
-            url: `http://39.105.217.228:9002/api/wmts/gettile/4b9006155b9946d5bf259dd750084f52/{z}/{x}/{y}`,
-            projection: getProjection('EPSG:3857')
-          }),
-          extent: [
-            13574731.18887278,
-            3469919.836022329,
-            13585338.647663247,
-            3478566.0198801826
-          ],
-          maxResolution: '19'
-        }),
-        new TileLayer({
-          source: new XYZ({
-            url: `${TIANDITU_URL}/DataServer?T=cia_w&x={x}&y={y}&l={z}&tk=111b0cbae5ef2fb8ebdf06f937b12dd8`,
-            projection: getProjection('EPSG:3857')
-          }),
-          minResolution: OUTDOOR_MIN_RESOLUTION
-        })
-      ],
       view: new View({
         projection: getProjection(DEFAULT_PROJECTION),
         zoom,
@@ -83,12 +50,10 @@ class OlMapLayer extends Component {
         minZoom
       })
     });
-
     this.map.getView().animate({
       center: DEFAULT_CENTER,
       duration: 0
     });
-
     this.setState({
       loaded: true
     });
--- src/map/openlayers/Outdoor.jsx
+++ src/map/openlayers/Outdoor.jsx
@@ -4,26 +4,97 @@ import React, { Component } from 'react';
 import TileLayer from 'ol/layer/Tile';
 import Overlay from 'ol/Overlay';
 import PropTypes from 'prop-types';
+import XYZ from 'ol/source/XYZ';
 import TileWMS from 'ol/source/TileWMS';
 import { fromEvent, from } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import { tap, filter, map, flatMap, toArray } from 'rxjs/operators';
 import { OUTDOOR_MIN_RESOLUTION } from './config';
-import { BASE_MAP_URL } from '../constant';
+import { BASE_MAP_URL, TIANDITU_URL } from '../constant';
+import { get as getProjection } from 'ol/proj.js';
 import popup from '../assets/img/popup.png';
 
 class OlOutdoorLayer extends Component {
   componentDidMount() {
-    this.layer = new TileLayer();
-    this.layer.setZIndex(0);
-    this.context.map.addLayer(this.layer);
+    this.loadTianDiTu();
+    this.loadCustomMap();
+    this.loadIndoorMaps();
+    this.initPopup();
+
+    window.hideOutdoorPopup = () => {
+      this.popupLayer.setPosition(null);
+    };
+
+    window.showIndoorMap = () => {
+      this.popupLayer.setPosition(null);
+      window.loadIndoor(this.data.sort((a, b) => a.floor - b.floor));
+    };
+  }
+
+  loadTianDiTu = () => {
+    this.context.map.addLayer(
+      new TileLayer({
+        source: new XYZ({
+          url: `${TIANDITU_URL}/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=111b0cbae5ef2fb8ebdf06f937b12dd8`,
+          projection: getProjection('EPSG:3857')
+        }),
+        zIndex: -10,
+        minResolution: OUTDOOR_MIN_RESOLUTION
+      })
+    );
+    this.context.map.addLayer(
+      new TileLayer({
+        source: new XYZ({
+          url: `${TIANDITU_URL}/DataServer?T=cia_w&x={x}&y={y}&l={z}&tk=111b0cbae5ef2fb8ebdf06f937b12dd8`,
+          projection: getProjection('EPSG:3857')
+        }),
+        zIndex: -5,
+        minResolution: OUTDOOR_MIN_RESOLUTION
+      })
+    );
+  };
+
+  loadCustomMap = () => {
+    this.context.map.addLayer(
+      new TileLayer({
+        source: new XYZ({
+          url: `http://39.105.217.228:9002/api/wmts/gettile/4b9006155b9946d5bf259dd750084f52/{z}/{x}/{y}`,
+          projection: getProjection('EPSG:3857')
+        }),
+        extent: [
+          13574731.18887278,
+          3469919.836022329,
+          13585338.647663247,
+          3478566.0198801826
+        ],
+        zIndex: -9,
+        maxResolution: '19'
+      })
+    );
+  };
+
+  loadIndoorMaps = () => {
+    this.source = new TileWMS({
+      url: `${BASE_MAP_URL}/indoor_map/wms`,
+      params: { LAYERS: 'indoor_map:indoor_map', TILED: true },
+      serverType: 'geoserver'
+    });
+    this.context.map.addLayer(
+      new TileLayer({
+        source: this.source,
+        zIndex: -4,
+        minResolution: OUTDOOR_MIN_RESOLUTION
+      })
+    );
+  };
+
+  initPopup = () => {
     this.popupLayer = new Overlay({
       element: this.ref,
       positioning: 'bottom-center',
       offset: [0, -10]
     });
     this.context.map.addOverlay(this.popupLayer);
-    this.loadIndoorMaps();
 
     fromEvent(this.context.map, 'singleclick')
       .pipe(
@@ -85,25 +156,6 @@ class OlOutdoorLayer extends Component {
         )
       )
       .subscribe();
-
-    window.hideOutdoorPopup = () => {
-      this.popupLayer.setPosition(null);
-    };
-
-    window.showIndoorMap = () => {
-      this.popupLayer.setPosition(null);
-      window.loadIndoor(this.data.sort((a, b) => a.floor - b.floor));
-    };
-  }
-
-  loadIndoorMaps = () => {
-    this.source = new TileWMS({
-      url: `${BASE_MAP_URL}/indoor_map/wms`,
-      params: { LAYERS: 'indoor_map:indoor_map', TILED: true },
-      serverType: 'geoserver'
-    });
-    this.layer.setSource(this.source);
-    this.layer.setMinResolution(OUTDOOR_MIN_RESOLUTION);
   };
 
   showPopup = (data) => {
